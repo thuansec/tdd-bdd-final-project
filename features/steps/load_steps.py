9394415b8e773614dc5e93051959c4c748a85417
thuansec <thuannguyen.sec@gmail.com>
2025-02-11 01:20:10 +0700

    update solution for task 5

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -50,3 +50,12 @@ def step_impl(context):
         #
         # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
         #
+        paylod = {
+            "name" : row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', 1],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=paylod)
+        assert(context.resp.status_code == HTTP_201_CREATED)
